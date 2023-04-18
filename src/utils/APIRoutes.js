b6b2cbf2aa78ace531a9c10c681b507dac2060d3
vigneshraaj <94341719+vigneshraaj19@users.noreply.github.com>
2023-04-18 13:58:38 +0530

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chat-app-server23.herokuapp.com";
+export const host = "https://chatapp-ycnw.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
